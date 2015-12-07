157d4f5cf3b2171b5e57b3f9c110d16199e7fb6a
Sarah Perez <karatezeus21@gmail.com>
2015-12-07 10:28:03 -0800

    cleaned and annotated methods as well as plots

--- video_plotter.py
+++ video_plotter.py
@@ -22,32 +22,33 @@ RAWDATA = '7557077831c1c6acd2.xml'
 
 DATA = os.path.join(PATH, SIMFOLDER, RAWDATA)
 
-
-DATEFMT = '%M:%S.%f'
-OUTDATEFMT = '%H:%M:%S.%f'
-ALL = []
+ALPHA = 0.2
 
 class Videolog(object):
-	"""docstring for Videolog"""
+	'''docstring for Videolog'''
 	def __init__(self, filename):
 		super(Videolog, self).__init__()
 		self.filename = filename
-
 		self.viewage = {}
 		self.subject = None
 		self.condition = self.find_condition()
 
 	def find_condition(self):
+		'''find condition of student from log file'''
 		textfile =self.filename.replace('.xml','_log.txt')
 		f = open(textfile,'r')
 		for line in f:
 			if "Condition 1" in line:
+				f.close()
 				return 1
 			if "Condition 2" in line:
+				f.close()
 				return 2
+		f.close()
 		return None
 
 	def update_subject(self):
+		'''identify subject of course'''
 		if self.viewage:
 			for videoname in self.viewage.keys():
 				if "rolley" in videoname:
@@ -62,7 +63,9 @@ class Videolog(object):
 		self.viewage[videoname] = viewcount
 		return None
 
+
 def collect_vlogs(path=PATH,folder=SIMFOLDER):
+	'''parse video viewage count for all logs in folder'''
 	allvlogs = []
 	os.chdir(os.path.join(path,folder))
 	for datafile in glob.glob("*.xml"):
@@ -70,9 +73,12 @@ def collect_vlogs(path=PATH,folder=SIMFOLDER):
 	return allvlogs
 
 def parsecount(count):
+	'''parse and format counts to ints'''
 	return [int(c) for c in count.split(',')]
 
 def parselog(datafile):
+	'''from a single video log file, '''
+	'''parse the view counts per video'''
 	vlog = Videolog(datafile)
 	tree = ET.parse(datafile)
 	root = tree.getroot()
@@ -80,7 +86,7 @@ def parselog(datafile):
 		video = child.attrib.values()
 		for subchild in child:
 			if subchild.tag == 'highlight':
-				###XXX
+				###editttt
 				continue
 			count = subchild.attrib.values()
 			if len(video) == 1 and len(count) == 1:
@@ -92,6 +98,7 @@ def parselog(datafile):
 	return vlog
 
 def get_metadata(vlogs):
+	'''create a dictionary of video names per subject'''
 	videos = {}
 	for vlog in vlogs:
 		if vlog.subject not in videos.keys():
@@ -102,6 +109,7 @@ def get_metadata(vlogs):
 	return videos
 
 def sanity_checks(vlogs):
+	'''check that vlog parsing is functioning'''
 	for l in vlogs:
 		if len(l.viewage) == 3 and l.subject != 'ENG':
 			print "AHHH"
@@ -110,50 +118,52 @@ def sanity_checks(vlogs):
 		print l.filename, l.subject, l.condition
 
 def plot_filling(x,y,name):
+	'''simple plot filling maker giving count data'''
 	fig, ax = plt.subplots(1)
-	ppl.fill_between(x, y, alpha = 0.3)
+	ppl.fill_between(x, y, facecolor='blue', alpha = ALPHA)
 	fig.savefig('plots/count_'+name+'.png')
 	return None
 
-def plot_mult_counts(vlogs, subject, videoname):
-
+def plot_mult_counts(vlogs, subject, condition, videoname):
+	'''plot multiple counts with different summary stats'''
+	'''for a paticular course subject, condition, and video'''
 	fig, ax = plt.subplots(1)
 	#files = []
 	counts = []
-	for cond in [1,2]:
-		for vlog in vlogs:
-			if vlog.condition == cond and vlog.subject == subject:
-				for video in vlog.viewage.keys():
-					if video == videoname:
-						y = vlog.viewage[videoname]
-						if sum(y)==0:
-							continue
-						x = range(len(y))
-						ppl.fill_between(x, y, alpha = 0.3, label=str(vlog.filename))
-						#files.append(vlog.filename)
-						counts.append(y)
-		if counts:
-			newcounts = zip(*counts)
-			medians = [stats.median(i) for i in newcounts]
-			ppl.plot(x,medians,'k-',label = 'median of '+str(len(counts))+' viewers')
-			means = [stats.mean(i) for i in newcounts]
-			ppl.plot(x,means,'w-',label = 'mean of '+str(len(counts))+' viewers')
-			ppl.legend()
-			# p = plt.Rectangle((0, 0), 1, 1, fc="r")
-			# ax.legend([p], files)
-			fig.savefig('plots/mult_count_'+subject+'_'+str(cond)+'_'+videoname+'.png')
+	for vlog in vlogs:
+		for video in vlog.viewage.keys():
+			if vlog.condition == condition and vlog.subject == subject and video == videoname:
+				y = vlog.viewage[videoname]
+				if sum(y)==0: #only take into account videos that were watched.
+					continue
+				x = range(len(y))
+				ppl.fill_between(x, y, facecolor='blue', alpha = ALPHA) #label=str(vlog.filename))
+				#files.append(vlog.filename)
+				counts.append(y)
+	if counts:
+		newcounts = zip(*counts)
+		medians = [stats.median(i) for i in newcounts]
+		ppl.plot(x,medians,'k-',label = 'median of '+str(len(counts))+' viewers')
+		means = [stats.mean(i) for i in newcounts]
+		ppl.plot(x,means,'w-',label = 'mean of '+str(len(counts))+' viewers')
+		ppl.legend()
+		# p = plt.Rectangle((0, 0), 1, 1, fc="r")
+		# ax.legend([p], files)
+		fig.savefig('plots/mult_count_'+subject+'_'+str(condition)+'_'+videoname+'.png')
+	else:
+		print "No counts found for under condition {0} for {1} video called {2}".format(condition,subject,videoname)
 	return None
 
 def plot_video_count(vlog, videoname):
+	'''given a user log and video, plot count'''
 	y = vlog.viewage[videoname]
 	x = range(len(y))
 	plot_filling(x,y,videoname+'_'+vlog.filename.replace('.xml',''))
 	return None
 
-def make_table(vlogs, output):
-	outfile = open(output,'w')
+def make_table(vlogs):
+	'''Output per user data into a table'''
 	lines = []
-	headers = ['studentID','condition','subject','videon','total 2 sec segments watched','total 2 sec segments rewatched']
 	for vlog in vlogs:
 		for videoname in vlog.viewage.keys():
 			line = []
@@ -164,25 +174,38 @@ def make_table(vlogs, output):
 			totalrepeat = sum([1 if x>1 else 0 for x in count])
 			line.append(totalrepeat)
 			lines.append(line)
+	return lines
 
+def write_table(headers, lines, output):
+	'''create file with tab delimited table'''
+	outfile = open(output,'w')
 	outfile.write('\t'.join(headers))
 	outfile.write('\n')
 	for line in lines:
 		outfile.write('\t'.join([str(l) for l in line]))
 		outfile.write('\n')
-
 	return None
 
-
-
 vlogs = collect_vlogs()
-outfile = 'table_watch_stats.txt'
-make_table(vlogs,outfile)
+all_videos = get_metadata(vlogs)
+
+for subject, videos in all_videos.iteritems():
+	for videoname in videos:
+		for condition in [1,2]:
+			print subject, condition, videoname
+			plot_mult_counts(vlogs, subject, condition, videoname)
 
 
-#all_videos = get_metadata(vlogs)
-#print all_videos
-#
+
+
+# outfile = 'table_watch_stats.txt'
+# headers = ['studentID','condition','subject','video','total 2 sec segments watched','total 2 sec segments rewatched']
+# table = make_table(vlogs)
+# write_table(headers, table, outfile)
+
+
+# print all_videos
+
 # for vlog in vlogs:
 # 	for video in vlog.viewage.keys():
 # 		plot_video_count(vlog,video)
