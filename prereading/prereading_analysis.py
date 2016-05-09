12f624f24d013940de151f039cd300435d1ad076
sperez8 <karatezeus21@gmail.com>
2016-05-09 16:12:15 -0700

    Finished dataframe merging

--- prereading/prereading_analysis.py
+++ prereading/prereading_analysis.py
@@ -8,45 +8,95 @@ import numpy as np
 import pandas as pd
 import numpy as np
 import sys
+import getpass
+from scipy import stats  
 
-df = pd.read_csv('Clean-data_160217_JBS_removed_empty_columns.csv')
+dfraw = pd.read_csv('C:\Users\\'+getpass.getuser()+'\Desktop\Clean-data_160217_JBS_removed_empty_columns.csv')
+dfsurvey = pd.read_csv('C:\Users\\'+getpass.getuser()+'\Desktop\Clean-data_addSurvey_160309_JBS_removed_empty_columns.csv')
 
 #please read this about unicode strings eventually: http://www.joelonsoftware.com/articles/Unicode.html
 
 # Make sure the data look as I expect
-
 #print df.describe()
 
 # Klugetastic: make a list, convert to an array and then a df
 datalist = []
-header = df.keys()
+surveylist = []
+#header = df.keys()
 topics = ['Blackbody','Masses','Resonance']
 treatments = ['TextThenPhet','TextOnly','PhetThenText']
 
 
-for index, row in df.iterrows():
-    # 'index' is the row number
-    # individual entries in the row can be indexed by row['ID'] as shown
-    
-    # In a given row, I want to iterate over the questions
+#merge with main dataframe
+dfmerged = pd.merge(left=dfraw,right=dfsurvey, left_on='anon-id', right_on='anon-id')
 
+phetquestion = {'Blackbody':'q01 - Approximately how long did you spend with the PhET simulation? (in minutes)',
+'Masses':'q5 - Approximately how long did you spend with the PhET simulation? (in minutes)','Resonance':'q6 - Approximately how long did you spend with the PhET simulation? (in minutes)'}
+textquestion = {'Blackbody':'q02 - Approximately how long did you spend reading the textbook sections? (in minutes)',
+'Masses':'q6 - Approximately how long did you spend reading the textbook sections? (in minutes)','Resonance':'q7 - Approximately how long did you spend reading the textbook sections? (in minutes)'}
+
+#now we iterate through the dataframe and creat a row per student x topic
+for index, row in dfmerged.iterrows():
     for topic in topics:
-    	datalist.append([row['anon-id'],row["Reading group"],row['TotalPrescore'],topic,row['Treatment.'+topic],row['PostScore.'+topic]])
-    # for iq in range(1,6):
-    #     solo = 'SOLO'+str(iq)        
-    #     group = 'GROUP'+str(iq)
-    #     post = 'POST'+str(iq) 
-    #     gpts = 'GPTS'+str(iq)
-    #     ### Not sure what this does
-    #     #if row[gr] > 0:
-    #     #    trt = 1
-    #     #else:
-    #     #    trt = 0
-        
-    #     #print row['ID'],row[post],row['GROUPID'],trt,row[gr],row[pre]
+    	datalist.append([str(row['anon-id']),
+                        str(row["Reading group_x"]),
+                        str(row['TotalPrescore']).replace('%',''),
+                        topic,
+                        row['Treatment.'+topic+'_x'],
+                        str(row['PostScore.'+topic]).replace('%',''),
+                        row[textquestion[topic]],
+                        row[phetquestion[topic]]
+                        ])
+    # for column in header:
+    #     #Check if questions
+    #     if re.match("q[0-9]+.*",q):
 
-    #     list2.append([row['ID'],row['GROUPID'],row['SOLOSCORE'],row['COURSESECTION'],
-    #                   row['DAYSUNTILPOST'],iq,row[solo],row[group],row[post],row[gpts]])
+# convert list2 to an array
+array2 = np.array(datalist)
+colnames = ['student id',"Reading group",'TotalPrescore','Topic','Treatment for topic','PostScore for Topic','Time on text for topic','Time on PhET for topic']
+# now make a dataframe out of it
+df = pd.DataFrame(array2,columns=colnames)
+
+#converting columns to numerical types
+for column in ['TotalPrescore','PostScore for Topic','Time on text for topic','Time on PhET for topic']:
+    df[column] = pd.to_numeric(df[column],errors='coerce')
+
+df['Engaged PhET'] = df['Time on PhET for topic'] > 5
+df['Engaged text'] = df['Time on text for topic'] > 20
+
+print df.describe()
+
+treatment1 = df[df["Engaged PhET"] == True]["PostScore.Masses"]
+treatment2 = df[df["Engaged PhET"] == False]["PostScore.Masses"]
+z_stat, p_val = stats.ranksums(treatment1, treatment2)  
+print "MWW RankSum P for treatments 1 and 2 =", p_val
 
 
-print datalist[0]
+
+
+# #First we grab the survey data, keeping the time on task
+# for index, row in dfsurvey.iterrows():
+#     for topic in topics:
+#         surveylist.append([row['anon-id'],
+#                         0,
+#                         0,
+                        # row['q5 - Approximately how long did you spend with the PhET simulation? (in minutes)'],
+                        # row['q6 - Approximately how long did you spend reading the textbook sections? (in minutes)'],
+                        # row['q6 - Approximately how long did you spend with the PhET simulation? (in minutes)'],
+                        # row['q7 - Approximately how long did you spend reading the textbook sections? (in minutes)'],
+#                         ])
+#     # for column in header:
+#     #     #Check if questions
+#     #     if re.match("q[0-9]+.*",q):
+
+# arraysurvey2 = np.array(surveylist)
+# # now make a dataframe out of it
+# colnames = ['anon-id','Time reading.Blackbody','Time PhET.Blackbody','Time reading.Masses','Time PhET.Masses','Time reading.Resonance','Time PhET.Resonance']
+
+
+
+# dfsurvey2 = pd.DataFrame(arraysurvey2,columns=colnames)
+
+
+
+
