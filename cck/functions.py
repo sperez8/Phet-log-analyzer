fff49e121444c69db1d589676e5424464d15e4da
Sarah Perez <karatezeus21@gmail.com>
2016-07-21 17:01:01 -0700

    Added code for markov chain and reorganized functions

--- cck/functions.py
+++ cck/functions.py
@@ -4,6 +4,7 @@ import sys
 import getpass
 import matplotlib
 import matplotlib.pyplot as plt
+import re
 
 df = pd.read_csv('C:\Users\\'+getpass.getuser()+'\\Google Drive\Sarah Ido folder\data\CCK\MATCHING_phet_cck_user_actions+sophistication_WITHPAUSE_anonymized.txt')
 df_scores = pd.read_csv('C:\Users\\'+getpass.getuser()+'\\Google Drive\Sarah Ido folder\data\CCK\MATCHING_phet_cck_user_data_anonymized.txt')
@@ -79,3 +80,107 @@ def get_circuits(df, students):
         for circuit in df[df["student"]==student].dropna().iterrows():
             circuits[student].add("".join([str(int(circuit[1][element])) for element in circuit_indices]))
     return circuits  
+
+def get_blocks(df, students, add_spaces = False):
+    '''gets blocks of sequences a list of students'''
+    def convert(action):
+        if action == 'Reset':
+            return 'X'
+        else: 
+            return action[0]
+    
+    blocks = {student:'S' for student in students}
+    for student in students:
+        sequence =  list(df[df['student']==student]['Family'])
+        block = re.sub(r'(.)\1+', r'\1',''.join([convert(action) for action in sequence]))
+        if add_spaces:
+            spaced_block = block[0]
+            for b in block:
+                spaced_block += ' '+b
+            blocks[student] += ' ' + spaced_block
+        else:
+            blocks[student] += block
+    return blocks
+
+def get_frequencies(blocks, shortest=3, longest=11):
+    frequencies = {student:Counter() for student in blocks.keys()}
+    for student,sequence in blocks.iteritems():
+        for seq_length in range(shortest, longest+1):  # loops through different possible sequence lengths
+            frequencies[student] += Counter(sequence[i:i+seq_length] for i in range(len(sequence)-seq_length-1))  # counts string matches for every string of the current length
+    return frequencies
+
+def remove_rare_frequencies(frequencies, N=2):
+    new_frequencies = copy.copy(frequencies)
+    for k in list(new_frequencies):
+            if new_frequencies[k] < N:
+                del new_frequencies[k]
+    return new_frequencies
+
+def remove_omni_frequencies(frequencies, N=96):
+    new_frequencies = copy.copy(frequencies)
+    for k in list(new_frequencies):
+            if new_frequencies[k] == N:
+                del new_frequencies[k]
+    return new_frequencies
+
+def clean_student_frequencies(all_frequencies, frequencies):
+    new_frequencies = {}
+    for student, freqs in frequencies.iteritems():
+        new_frequencies[student] = freqs & all_frequencies
+    return new_frequencies
+
+def keep_frequencies_with(frequencies,keep='P'):
+    new_frequencies = copy.copy(frequencies)
+    for k in list(new_frequencies):
+        if keep not in k:
+            del new_frequencies[k]
+    return new_frequencies
+
+def select_frequencies(frequencies, attribute, level):
+    new_frequencies = {}
+    '''gets frequencies of students given an attribute of the student'''
+    relevant_students =  set(df_scores[df_scores[attribute]==level]['student'])
+    for student, f in frequencies.iteritems():
+        if student in relevant_students:
+            new_frequencies[student] = f
+    return new_frequencies
+
+def mega_process(activity='a1', level='scaffolding', value1='scaff', value2='not', shortest=3, longest=10, keep = None):
+    df2 = df[df.Activity == activity]
+    blocks = get_blocks(df2,get_students())
+    frequencies = get_frequencies(blocks, shortest=shortest, longest=longest)
+
+    sum_frequencies = sum(frequencies.values(), Counter())
+    counts_frequencies = Counter({f:sum([ 1 if f in freq else 0 for freq in frequencies.values()]) for f in list(sum_frequencies)})
+
+#     # Remove frequencies done by few and all students
+#     counts_frequencies = remove_rare_frequencies(counts_frequencies,3)
+#     counts_frequencies = remove_omni_frequencies(counts_frequencies,len(get_students()))
+
+#     # update student frequencies by removing those removed in the collection of all frequencies
+#     frequencies = clean_student_frequencies(counts_frequencies, frequencies)
+    
+    if keep:
+        counts_frequencies = keep_frequencies_with(counts_frequencies,keep=keep)
+        frequencies = clean_student_frequencies(counts_frequencies, frequencies)
+    
+    f1 = select_frequencies(frequencies,level,value1)
+    f2 = select_frequencies(frequencies,level,value2)
+
+    sum_frequencies = sum(f1.values(), Counter())
+    count1 = Counter({f:sum([ 1 if f in freq else 0 for freq in f1.values()]) for f in list(sum_frequencies)})
+    sum_frequencies = sum(f2.values(), Counter())
+    count2 = Counter({f:sum([ 1 if f in freq else 0 for freq in f2.values()]) for f in list(sum_frequencies)})
+
+    return sum_frequencies, count1, count2
+
+def difference(seqs1,seqs2,N=10):
+    diff = (seqs1-seqs2).most_common(N)
+    print "Sequence: count = seq1 - seq2\n-----------------------------"
+    for seq,count in diff:
+        print "{0}: \t {1} = {2} - {3} ".format(seq, count, seqs1[seq], seqs2[seq])
+    
+    diff = (seqs2-seqs1).most_common(N)
+    print "\nSequence: count = seq2 - seq1\n----------------------------"
+    for seq,count in diff:
+        print "{0}: \t {1} = {2} - {3} ".format(seq, count, seqs2[seq], seqs1[seq])
